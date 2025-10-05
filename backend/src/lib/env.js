a9d399e0a69ff05e3ecb29baf1e289d328ae884e
kishan5459 <kishandudhat2315@gmail.com>
2025-10-05 18:09:03 +0530

    completed

---
+++ backend/src/lib/env.js
@@ -0,0 +1,21 @@
+import "dotenv/config";
+
+export const ENV = {
+  PORT: process.env.PORT,
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  NODE_ENV: process.env.NODE_ENV,
+  CLIENT_URL: process.env.CLIENT_URL,
+  SMTP_HOST: process.env.SMTP_HOST,
+  SMTP_SERVICE: process.env.SMTP_SERVICE,
+  SMTP_PORT: process.env.SMTP_PORT,
+  SMTP_MAIL: process.env.SMTP_MAIL,
+  SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+  EMAIL_FROM: process.env.EMAIL_FROM,
+  EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  ARCJET_KEY: process.env.ARCJET_KEY,
+  ARCJET_ENV: process.env.ARCJET_ENV,
+};
